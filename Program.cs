75cf57bcf361bd68abdea6bc541f5f6a1fb3c6c3
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-05-11 13:47:08 -0400

    refactor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AirlineSeats
 {
@@ -10,23 +11,46 @@ namespace AirlineSeats
             bool isAvailable;
 
             public Seat(string seatNumber, SeatPosition seatPosition, bool isAvailable){
-                seatNumber = seatNumber;
-                seatPosition = seatPosition;
-                isAvailable = isAvailable;
+                this.seatNumber = seatNumber;
+                this.seatPosition = seatPosition;
+                this.isAvailable = isAvailable;
             }
             
         }
 
-        enum SeatPosition {
+        public enum SeatPosition {
             Aisle,
             Window,
             Middle,
         }
         
-        class Plane {
+        public class Plane {
             string TailNumber;
             string FlightNumber;
-            Seat Seats;
+            
+            List<Seat> Seats;
+
+            public Plane(string tailNum, string flightNum){
+                this.TailNumber = tailNum;
+                this.FlightNumber = flightNum;
+
+                string[] characterLiterals = {"a", "b", "c", "d", "e", "f"};
+
+                for(int x = 1; x <= 10; x++){
+                    for(int y = 0; y <= characterLiterals.Length; y++){
+                        if(y == 0 || y == 5){
+                            Seats.Add(new Seat(x.ToString() + characterLiterals[y], SeatPosition.Window, true));
+                        }
+                        else if(y == 1 || y == 4){
+                            Seats.Add(new Seat(x.ToString() + characterLiterals[y], SeatPosition.Middle, true));
+                        }
+                        else if(y == 2 || y == 3){
+                            Seats.Add(new Seat(x.ToString() + characterLiterals[y], SeatPosition.Aisle, false));
+                        }
+                    }
+                }
+            }
+
         }
 
         static void Main(string[] args)
